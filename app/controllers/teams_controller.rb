6ea0bde6a72e3e22f271b5913528bbe8787f7641
Leon <hualiang.tian@gmail.com>
2021-10-12 23:20:18 -0400

    adding create action for team controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,4 +3,19 @@ class TeamsController < ApplicationController
         teams = Team.all
         render json: teams
     end
+
+    def create
+        team = Team.new(team_params) 
+        # byebug
+        if team.save
+            render json: team
+        else 
+            render json: {error: "Team Creation failed, please try again"}
+        end   
+    end
+
+    private
+    def tam_params
+        params.require(:team).permit(:name, :leader, :description)
+    end
 end
